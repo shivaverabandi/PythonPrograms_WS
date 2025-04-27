90e4a4dd81446ec44b6a57b3f4131a7e5df8928a
Shiva Verabandi <vsivasai07@gmail.com>
2025-04-27 21:34:09 +0530

    added reverse str

--- Recursion/reverseSring.py
+++ Recursion/reverseSring.py
@@ -1,10 +1,45 @@
 
 
+"""
+Base Case Termination:
+
+If current index idx equals string length len(s):
+
+Return the accumulated result ans (now fully reversed)
+
+Recursive Case:
+
+Take the current character s[idx]
+
+Prepend it to the accumulator: s[idx] + ans
+
+Recur with:
+
+Same string s
+
+Next index idx + 1
+
+New accumulator s[idx] + ans
+
+Step-by-Step Execution for "abc":
+
+Call Stack	idx	s[idx]	ans (Before)	ans (After)
+
+1st call	0	'a'	""	"a"
+
+2nd call	1	'b'	"a"	"ba"
+
+3rd call	2	'c'	"ba"	"cba"
+
+4th call	3	(end)	"cba"	Returns "cba"
+
+"""
 
 def reverse_str(s, idx=0, ans=""):
     if idx == len(s):
         return ans
     return reverse_str(s, idx+1, s[idx] + ans) 
+
 
 str_input = "mani"
 print(reverse_str(str_input)) 
